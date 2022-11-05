6849029157716ae458ae3ac7e2fb5c2a14103a38
observer <denis.bernardes099@gmail.com>
2022-11-05 02:00:27 -0300

    The bugs of Full memory and the COMMENT keyword were fixed

--- builds/ACS/data/header_routines.py
+++ builds/ACS/data/header_routines.py
@@ -13,10 +13,10 @@ vertical_clock_amp = ['Normal', '+1', '+2', '+3', '+4']
 acquisition_mode = ['Single', 'Accumulate', "Kinetic"]
 
 
-ss = pd.read_csv('header_content.csv', delimiter='\t')
-cards = [(keyword, '', comment)
-         for keyword, comment in zip(ss['Keyword'], ss['Comment'])]
-header_content = fits.Header(cards)
+# ss = pd.read_csv('header_content.csv', delimiter='\t')
+# cards = [(keyword, '', comment)
+#          for keyword, comment in zip(ss['Keyword'], ss['Comment'])]
+# header_content = fits.Header(cards)
 
 
 def reformat_string(string):
@@ -47,6 +47,10 @@ def get_read_noise(index, serial_number):
 
 
 def save_image(file, data, channel_information):
+    ss = pd.read_csv('header_content.csv', delimiter='\t')
+    cards = [(keyword, '', comment)
+             for keyword, comment in zip(ss['Keyword'], ss['Comment'])]
+    header_content = fits.Header(cards)
     file = reformat_string(file)
     channel_information = json.loads(reformat_string(channel_information))
     for key, value in channel_information.items():
